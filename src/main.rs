61c878d51466591afdc46d24921b233a72674a10
rp620 <rushil.patel20@imperial.ac.uk>
2022-04-12 18:07:28 +0100

    feat: added function to calculate double owl in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn calcDoubleOwl(x: u32, y: u32) -> u32 {
+    /* 2 ^ x * ((2 * y) + 1)) */
+
+    let base: u32 = 2;
+    base.pow(x) * ((2 * y) + 1)
+}
+
 fn main() {
     println!("Hello, world!");
 }
