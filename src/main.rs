33e55db13c700aa2fa836140460372513c3ac160
rp620 <rushil.patel20@imperial.ac.uk>
2022-04-12 18:15:44 +0100

    refactor: added printing around calculations to show steps

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 fn calcDoubleOwl(x: u32, y: u32) -> u32 {
-    /* 2 ^ x * ((2 * y) + 1)) */
+    /* 2 ^ x * ((2 * y) + 1) */
+
+    println!("Calculating double owl for {}, {}", x, y);
 
     let base: u32 = 2;
-    base.pow(x) * ((2 * y) + 1)
+    let res = base.pow(x) * ((2 * y) + 1);
+
+    println!("<<{}, {}>> = 2^{} * (2 * {} + 1) = {}", x, y, x, y, res);
+    res
 }
 
 fn calcSingleOwl(x: u32, y: u32) -> u32 {
-    calcDoubleOwl(x, y) - 1
+    /* (2 ^ x * ((2 * y) + 1) - 1) */
+
+    println!("Calculating single owl for {}, {}", x, y);
+    let res = calcDoubleOwl(x, y) - 1;
+    println!("Therefore, single owl is: <{}, {}> = {}", x, y, res);
+    res
 }
+
+
 
 fn main() {
     println!("Hello, world!");
