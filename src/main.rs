0f2aaa9867699832e6d0b001e342a4b2a0447519
rp620 <rushil.patel20@imperial.ac.uk>
2022-04-12 18:54:52 +0100

    refactor: moved encoding into its own .rs file

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,4 @@
-fn calcDoubleOwl(x: u32, y: u32) -> u32 {
-    /* 2 ^ x * ((2 * y) + 1) */
-
-    println!("Calculating double owl for {}, {}", x, y);
-
-    let base: u32 = 2;
-    let res = base.pow(x) * ((2 * y) + 1);
-
-    println!("<<{}, {}>> = 2^{} * (2 * {} + 1) = {}", x, y, x, y, res);
-    res
-}
-
-fn calcSingleOwl(x: u32, y: u32) -> u32 {
-    /* (2 ^ x * ((2 * y) + 1) - 1) */
-
-    println!("Calculating single owl for {}, {}", x, y);
-    let res = calcDoubleOwl(x, y) - 1;
-    println!("Therefore, single owl is: <{}, {}> = {}", x, y, res);
-    res
-}
-
-fn reg_halt() -> u32{
-    return 0
-}
-
-fn reg_subtract(i: u32, j: u32, k: u32) -> u32 {
-    /* Ri- -> Lj, Lk converts to:
-            << 2i + 1, <j, k> >>*/
-    println!("CALCULATING SUBTRACT!");
-    println!("Calculating single owl, for second arg in subtract's double owl");
-    let snd = calcSingleOwl(j, k);
-    let res = calcDoubleOwl(2*i + 1, snd);
-    println!("R{}- -> L{}, L{} converts to {}", i, j, k, res);
-    res
-}
-
-fn reg_add(i: u32, j: u32) -> u32 {
-    /* Ri+ -> Lj converts to:
-        <<2i, j>> */
-
-    println!("CALCULATING ADD!");
-    let res = calcDoubleOwl(2*i, j);
-    println!("R{}+ -> L{} converts to {}", i, j, res);
-    res
-}
-
-fn list_encoding(nums: Vec<u32>) -> u32 {
-
-    /*  Empty list => 0
-        x : l => <<x, `l`>>, where `l` is the rest of the encoded list */
-    println!("ENCODING A LIST");
-
-    if let Some(head) = nums.get(0) {
-        //list is not empty
-        if nums.len() > 1 {
-            //list is not singleton
-            let tail = list_encoding(nums[1..].to_vec());
-            return calcDoubleOwl(*head, tail)
-        } else {
-            //list is singleton
-            return calcDoubleOwl(*head, 0);
-        }
-    } else {
-        //list is empty
-        return 0
-    }
-
-}
-
-
+mod encode;
 
 fn main() {
     /* to encode programs:
@@ -77,6 +8,9 @@ fn main() {
         convert entire vector when finished using list_encoding()
 
     */
+
+    let res = encode::reg_subtract(2, 3, 4);
+    println!("{}", res);
 
     println!("Hello, world!");
 }
