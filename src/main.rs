f997bc45a967739f2399b0b6ed05bb97a391968c
rp620 <rushil.patel20@imperial.ac.uk>
2022-04-12 18:09:12 +0100

    feat: implemented calcSingleOwl in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ fn calcDoubleOwl(x: u32, y: u32) -> u32 {
     base.pow(x) * ((2 * y) + 1)
 }
 
+fn calcSingleOwl(x: u32, y: u32) -> u32 {
+    calcDoubleOwl(x, y) - 1
+}
+
 fn main() {
     println!("Hello, world!");
 }
