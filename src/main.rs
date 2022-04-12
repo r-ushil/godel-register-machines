5fef887bebcdae9e39869d0e51ba5c0e333da0d0
rp620 <rushil.patel20@imperial.ac.uk>
2022-04-12 18:46:01 +0100

    refactor: added usage comment in main()

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn calcSingleOwl(x: u32, y: u32) -> u32 {
     res
 }
 
-fn reg_subtract(i: u32, j: u32, k: u32) {
+fn reg_subtract(i: u32, j: u32, k: u32) -> u32 {
     /* Ri- -> Lj, Lk converts to:
             << 2i + 1, <j, k> >>*/
     println!("CALCULATING SUBTRACT!");
@@ -27,22 +27,23 @@ fn reg_subtract(i: u32, j: u32, k: u32) {
     let snd = calcSingleOwl(j, k);
     let res = calcDoubleOwl(2*i + 1, snd);
     println!("R{}- -> L{}, L{} converts to {}", i, j, k, res);
+    res
 }
 
-fn reg_add(i: u32, j: u32) {
+fn reg_add(i: u32, j: u32) -> u32 {
     /* Ri+ -> Lj converts to:
         <<2i, j>> */
 
     println!("CALCULATING ADD!");
     let res = calcDoubleOwl(2*i, j);
     println!("R{}+ -> L{} converts to {}", i, j, res);
+    res
 }
 
 fn list_encoding(nums: Vec<u32>) -> u32 {
 
     /*  Empty list => 0
         x : l => <<x, `l`>>, where `l` is the rest of the encoded list */
-        
     println!("ENCODING A LIST");
 
     if let Some(head) = nums.get(0) {
@@ -65,5 +66,13 @@ fn list_encoding(nums: Vec<u32>) -> u32 {
 
 
 fn main() {
+    /* to encode programs:
+
+        convert instruction into it's Godel number using add/subtract
+        add this number to a vector
+        convert entire vector when finished using list_encoding()
+
+    */
+
     println!("Hello, world!");
 }
