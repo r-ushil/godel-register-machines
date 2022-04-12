3813bcabcac46fc870a170bdbe88dc8dd3926889
rp620 <rushil.patel20@imperial.ac.uk>
2022-04-12 17:51:02 +0100

    chore: new cargo project setup

---
+++ target/debug/deps/godel_reg_machines-e83a2e08c5cb3e75.d
@@ -0,0 +1,5 @@
+/home/rushil791/Year2/Models/godel-register-machines/target/debug/deps/godel_reg_machines-e83a2e08c5cb3e75: src/main.rs
+
+/home/rushil791/Year2/Models/godel-register-machines/target/debug/deps/godel_reg_machines-e83a2e08c5cb3e75.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/godel-reg-machines.d
@@ -0,0 +1,1 @@
+/home/rushil791/Year2/Models/godel-register-machines/target/debug/godel-reg-machines: /home/rushil791/Year2/Models/godel-register-machines/src/main.rs
